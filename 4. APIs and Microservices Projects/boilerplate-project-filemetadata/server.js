2d0bdc9fb4cf0d813d5a9d1c39e3ff9affdecfa6
Jeremiah Bassey Asuquo <31101804+jerryasuquo@users.noreply.github.com>
2021-01-01 17:11:30 +0100

    Adding initial files

---
+++ 4. APIs and Microservices Projects/boilerplate-project-filemetadata/server.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const upload = multer();
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.set("view engine", "pug");
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  return res.json({ name: originalname, type: mimetype, size: size });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
